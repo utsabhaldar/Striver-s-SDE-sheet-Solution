6f148819e1ec10cccd63c7d072b3fd9a1c816c1b
Utsab Haldar <utsabhaldar88888@gmail.com>
2023-09-01 21:16:21 +0530

    Binary Search - Coding Ninja - find nth root of m - Java

---
+++ Binary Search/cpdingninja_find_nth_root_of_m.java
@@ -0,0 +1,29 @@
+// Striver's SDE Sheet - Binary Search - Coding Ninja - find nth root of m - Java
+
+// Time:O(log n * log m)
+public class cpdingninja_find_nth_root_of_m {
+    public static int func(int mid, int n, int m) {
+        long ans = 1;
+        for (int i = 1; i <= n; i++) {
+            ans = ans * mid;
+            if (ans > m) return 2;
+        }
+        if (ans == m) return 1;
+        return 0;
+    }
+    public static int NthRoot(int n, int m) {
+        int low = 1, high = m;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int midN = func(mid, n, m);
+            if (midN == 1) {
+                return mid;
+            } else if (midN == 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
