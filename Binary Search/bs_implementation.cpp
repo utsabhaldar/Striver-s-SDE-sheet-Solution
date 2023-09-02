f742e6fe81239ee48607702492f41d2a316db203
Utsab Haldar <utsabhaldar88888@gmail.com>
2023-09-02 19:00:22 +0530

    Binary Search - leetcode 704 - C++, Java

--- Binary Search/bs_implementation.cpp
+++ Binary Search/bs_implementation.cpp
@@ -1,5 +1,6 @@
-// Striver's SDE Sheet - Binary Search implementation - C++, Java
+// Striver's SDE Sheet - Binary Search implementation - leetcode 704 - C++, Java
 
+// C++
 // Time: O(log N base 2)
 int bs(vector<int>& nums, int target, int low, int high){
     if(low>high) return -1;
@@ -13,4 +14,19 @@ int search(vector<int> &nums, int target) {
 
     return bs(nums, target, 0, nums.size()-1);
 
+}
+
+// Java
+class Solution {
+    static int bs(int[] nums, int target, int low, int high){
+    if(low>high) return -1;
+    int mid = low + (high - low)/2;
+    if(nums[mid] == target) return mid;
+    else if(target > nums[mid]) return bs(nums, target, mid+1, high);
+    return bs(nums, target, low, mid-1);
+    }
+    
+    public int search(int[] nums, int target) {
+        return bs(nums, target, 0, nums.length-1);
+    }
 }
