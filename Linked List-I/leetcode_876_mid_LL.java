9e51a0baef3347670ad6ce6d8a8469912ed3b22a
Utsab Haldar <utsabhaldar88888@gmail.com>
2023-09-04 12:30:19 +0530

    Linked List 1 - Leetcode Q876 - Java

---
+++ Linked List-I/leetcode_876_mid_LL.java
@@ -0,0 +1,40 @@
+// Striver's SDE Sheet - Linked List 1 - Leetcode Q876 - Java
+
+// 876. Middle of the Linked List
+
+// Given the head of a singly linked list, return the middle node of the linked list.
+// If there are two middle nodes, return the second middle node.
+
+// Example 1:
+// Input: head = [1,2,3,4,5]
+// Output: [3,4,5]
+// Explanation: The middle node of the list is node 3.
+// Example 2:
+// Input: head = [1,2,3,4,5,6]
+// Output: [4,5,6]
+// Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+
+// Constraints:
+// The number of nodes in the list is in the range [1, 100].
+// 1 <= Node.val <= 100
+
+
+
+// Time:O(n/2)
+// Space:O(1)
+public class leetcode_876_mid_LL {
+    /*
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow, fast;
+        slow  = fast = head;
+        while(fast != null  && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow; 
+    }
+
+    */
+}
