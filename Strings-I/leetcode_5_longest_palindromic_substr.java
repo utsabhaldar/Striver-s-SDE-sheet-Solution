55e2e121818502b478dd56b002dea90fafb1abf4
Utsab Haldar <utsabhaldar88888@gmail.com>
2023-09-10 23:55:12 +0530

    String 1 - Leetcode Q5 - Java

---
+++ Strings-I/leetcode_5_longest_palindromic_substr.java
@@ -0,0 +1,49 @@
+// Striver's SDE Sheet - String 1 - Leetcode Q5 - Java
+
+// 5. Longest Palindromic Substring
+
+// Given a string s, return the longest palindromic substring in s.
+
+// Example 1:
+// Input: s = "babad"
+// Output: "bab"
+// Explanation: "aba" is also a valid answer.
+// Example 2:
+// Input: s = "cbbd"
+// Output: "bb"
+ 
+// Constraints: 
+// 1 <= s.length <= 1000
+// s consist of only digits and English letters.
+
+
+
+// Time:O(n + 2n)
+// Space:O(1)
+public class leetcode_5_longest_palindromic_substr {
+    int maxLen = 0;
+    int lo = 0;
+    public String longestPalindrome(String s) {
+        char[] input = s.toCharArray();
+        if(s.length() < 2) {
+            return s;
+        }
+        
+        for(int i = 0; i<input.length; i++) {
+            expandPalindrome(input, i, i);
+            expandPalindrome(input, i, i+1);
+        }
+        return s.substring(lo, lo+maxLen);
+    }
+    
+    public void expandPalindrome(char[] s, int j, int k) {
+        while(j >= 0 && k < s.length && s[j] == s[k]) {
+            j--;
+            k++;
+        }
+        if(maxLen < k - j - 1) {
+            maxLen = k - j - 1;
+            lo = j+1;
+        }
+    }
+}
