c942d1893a346d9872fba58efd64d9a6d8c00ab4
Utsab Haldar <utsabhaldar88888@gmail.com>
2023-09-23 10:08:21 +0530

    Graph 1 - leetcode Q133 - Java

--- Graph-I/leetcode_133_clone_graph.java
+++ Graph-I/leetcode_133_clone_graph.java
@@ -9,8 +9,7 @@
 //     public int val;
 //     public List<Node> neighbors;
 // }
-// Test case format:
-// For simplicity, each node's value is the same as the node's index (1-indexed). For example, the first node with val == 1, the second node with val == 2, and so on. The graph is represented in the test case using an adjacency list.
+// Test case format: For simplicity, each node's value is the same as the node's index (1-indexed). For example, the first node with val == 1, the second node with val == 2, and so on. The graph is represented in the test case using an adjacency list.
 // An adjacency list is a collection of unordered lists used to represent a finite graph. Each list describes the set of neighbors of a node in the graph.
 // The given node will always be the first node with val = 1. You must return the copy of the given node as a reference to the cloned graph.
 // Example 1:Input: adjList = [[2,4],[1,3],[2,4],[1,3]]
