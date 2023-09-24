9d08d864ad14302702d2f0220ff7071627c0c381
Utsab Haldar <utsabhaldar88888@gmail.com>
2023-09-24 12:04:28 +0530

    Linked List & Arrays - Leetcode Q485 - Java

---
+++ Linked List &  Arrays/leetcode_485_max_consecutive_ones.java
@@ -0,0 +1,35 @@
+// Striver's SDE Sheet - Linked List & Arrays - Leetcode Q485 - Java
+
+// 485. Max Consecutive Ones
+
+// Given a binary array nums, return the maximum number of consecutive 1's in the array.
+// Example 1:
+// Input: nums = [1,1,0,1,1,1]
+// Output: 3
+// Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+// Example 2:
+// Input: nums = [1,0,1,1,0,1]
+// Output: 2
+// Constraints:
+// 1 <= nums.length <= 105
+// nums[i] is either 0 or 1.
+
+
+
+// Time:O(n)
+// Space:O(1)
+public class leetcode_485_max_consecutive_ones {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int max = 0;
+        for(int i = 0; i< nums.length; i++){
+            if(nums[i] == 1){
+                count++;
+                if(count>max) max=count;
+            }else{
+                count = 0;
+            }
+        }
+        return max;
+    }
+}
