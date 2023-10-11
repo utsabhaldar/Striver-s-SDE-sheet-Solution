677eb6e4798c48178baf9689337c9067b21b8d42
Utsab Haldar <utsabhaldar88888@gmail.com>
2023-10-12 01:28:45 +0530

    Array3 - Leetcode Q493 - Java

---
+++ Arrays Part-III/leetcode_493_reverse_pairs.java
@@ -0,0 +1,79 @@
+// Striver's SDE Sheet - Array3 - Leetcode Q493 - Java
+
+// 493. Reverse Pairs
+
+// Given an integer array nums, return the number of reverse pairs in the array.
+// A reverse pair is a pair (i, j) where:
+// 0 <= i < j < nums.length and
+// nums[i] > 2 * nums[j].
+// Example 1:
+// Input: nums = [1,3,2,3,1]
+// Output: 2
+// Explanation: The reverse pairs are:
+// (1, 4) --> nums[1] = 3, nums[4] = 1, 3 > 2 * 1
+// (3, 4) --> nums[3] = 3, nums[4] = 1, 3 > 2 * 1
+// Example 2:
+// Input: nums = [2,4,3,5,1]
+// Output: 3
+// Explanation: The reverse pairs are:
+// (1, 4) --> nums[1] = 4, nums[4] = 1, 4 > 2 * 1
+// (2, 4) --> nums[2] = 3, nums[4] = 1, 3 > 2 * 1
+// (3, 4) --> nums[3] = 5, nums[4] = 1, 5 > 2 * 1
+// Constraints:
+// 1 <= nums.length <= 5 * 104
+// -231 <= nums[i] <= 231 - 1
+
+
+
+// Time:O(2n*logn)
+// Space:O(n)
+public class leetcode_493_reverse_pairs {
+    public int reversePairs(int[] nums) {
+        return mergeSort(nums, 0, nums.length - 1);
+    }
+
+    public int mergeSort(int[] arr, int low, int high) {
+        int cnt = 0;
+        if (low >= high) return cnt;
+        int mid = (low + high) / 2;
+        cnt += mergeSort(arr, low, mid);
+        cnt += mergeSort(arr, mid + 1, high);
+        cnt += countPairs(arr, low, mid, high);
+        merge(arr, low, mid, high);
+        return cnt;
+    }
+
+    public void merge(int[] arr, int low, int mid, int high) {
+        int[] temp = new int[high - low + 1];
+        int left = low;
+        int right = mid + 1;
+        int index = 0;
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp[index++] = arr[left++];
+            } else {
+                temp[index++] = arr[right++];
+            }
+        }
+        while (left <= mid) {
+            temp[index++] = arr[left++];
+        }
+        while (right <= high) {
+            temp[index++] = arr[right++];
+        }
+        System.arraycopy(temp, 0, arr, low, temp.length);
+    }
+
+    public int countPairs(int[] arr, int low, int mid, int high) {
+        int right = mid + 1;
+        int cnt = 0;
+        for (int i = low; i <= mid; i++) {
+            while (right <= high && (long) arr[i] > 2L * arr[right]) {
+                right++;
+            }
+            cnt += (right - (mid + 1));
+        }
+        return cnt;
+    }
+}
