5e192763dbcc581fc2a855c1a7d4208117ea66ad
Utsab Haldar <utsabhaldar88888@gmail.com>
2023-11-17 09:07:46 +0530

    Binary Tree Postorder Traversal

---
+++ Binary Trees Part-I/Q145_postorder_traversal.java
@@ -0,0 +1,46 @@
+// Striver's SDE Sheet - Binary Trees 1 - Leetcode - Java
+
+// 145. Binary Tree Postorder Traversal
+
+// Given the root of a binary tree, return the postorder traversal of its nodes' values.
+// Example 1:
+// Input: root = [1,null,2,3]
+// Output: [3,2,1]
+// Constraints:
+// The number of the nodes in the tree is in the range [0, 100].
+// -100 <= Node.val <= 100
+
+
+
+// Time:O(n)
+// Space:O(n)
+import java.util.*;
+
+public class Q145_postorder_traversal {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    private void postorder(TreeNode root, List<Integer> ans){
+        if(root != null){
+            postorder(root.left, ans);
+            postorder(root.right, ans);
+            ans.add(root.val);
+        }
+    }
+        
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+
+        postorder(root, ans);
+        return ans;
+    }
+}
